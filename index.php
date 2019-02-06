d07c55b2161a2c5631c24c1073d7a9bd03769e2a
cremi <cremi@Dell-14.rouviere-gretavarmed.fr>
2019-02-06 14:06:22 +0000

    Rajout fichier custom.ccs + première regles de custom

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <link rel="icon" href="favicon.ico">
         <title>Portfolio d'Amélia Pilou (BTS SIO 2016)</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" />
+        <link href="css/custom.css" rel="stylesheet" />
     </head>
     <body>
         <nav class="navbar navbar-inverse navbar-fixed-top">
